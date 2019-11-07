58337851d8a711a0a0c651d0148e9590c661f8b0
shyampandya97@gmail.com <shyampandya97@gmail.com>
2019-11-07 13:42:08 +0530

    Live attendee monitoring

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from res import id as id, constants as constants
 from websocket_server import WebsocketServer
-from StrategyMain import examine_strategies
 from threading import Thread
 from time import time, sleep
-import websocket
 import sched
-import json
+import simplejson as json
+import boto3
 
-
-strategyDict = {}
 scheduler = sched.scheduler(time, sleep)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('PeopleCounter')
 
-class PopoServer(Thread):
+class AttendeeServer(Thread):
     clients = {}
+    broadcast_counter = 0
 
     # Called for every client connecting (after handshake)
     def new_client(self, client, server):
@@ -30,153 +30,41 @@ class PopoServer(Thread):
         print("Client(%d) said: %s" % (client['id'], message))
 
     def broadcast(self):
-        strategyList = []
-        self.format(strategyList)
-        self.all = self.server.send_message_to_all(json.dumps(strategyList))
-
-    def format(self, strategyList):
-        for key, value in strategyDict.items():
-            strategy = {}
-            data = []
-            strategy[id.name] = key
-            for i in range(0,len(value)):
-                dataInner = {}
-                for key2, value2 in value[i].items():
-                    coinKey = key2.split(':')
-                    time_frame = coinKey[1]
-                    coin = coinKey[2][1:]
-                    dataInner[id.time] = time_frame
-                    dataInner[id.From] = coin[:3]
-                    dataInner[id.to] = coin[3:]
-                    dataInner[id.data] = value2
-                data.append(dataInner)
-            strategy[id.data] = data
-            strategyList.append(strategy)
-        print('done')
+        response = table.scan()
+        items = response['Items']
+        while True:
+            print('Total items: ' + str(len(response['Items'])))
+            if response.get('LastEvaluatedKey'):
+                response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+                items += response['Items']
+            else:
+                break
+        self.broadcast_counter += 1
+        print('Broadcast number: ', self.broadcast_counter)
+        self.all = self.server.send_message_to_all(json.dumps(items))
 
     def run(self):
-        self.server = WebsocketServer(host=constants.server[id.host], port=constants.server[id.port])
+        self.server = WebsocketServer(host=constants.server[id.host], port=9006) #constants.server[id.port])
         self.server.set_fn_new_client(self.new_client)
         self.server.set_fn_client_left(self.client_left)
         self.server.set_fn_message_received(self.message_received)
         self.server.run_forever()
 
+class DataFetcher(Thread):
 
-class BitfinexClient(Thread):
-    channelKeys = {}
-    candleData = {}
-
-
-
-    def removeStaleData(self, chanId):
-        no_removed = len(self.candleData[chanId]) - constants.history[id.history_size]
-        for i in range(0, no_removed):
-            del self.candleData[chanId][i]
-
-    def on_message(self, message):
-        newMessage = json.loads(message)
-
-        if id.serverId in newMessage or (id.event in newMessage and newMessage[id.event]== id.error):
-            print('Ignore')
-
-        elif id.chanId in newMessage:
-            if newMessage[id.chanId] not in self.channelKeys:
-                self.channelKeys[newMessage[id.chanId]] = newMessage[id.key]
-        else:
-            try:
-                chanId = newMessage[0]
-            except:
-                print('Error')
-            data = newMessage[1]
-            if data and type(data) == list:
-                if type(data[0]) == list:
-                    data = sorted(data, key=lambda k: k[0])
-                if chanId not in self.candleData:
-                    self.candleData[chanId] = []
-                    for value in data:
-                        x = {}
-                        if type(data[0]) == list:
-                            x[value[0]] = value
-                            self.candleData[chanId].append(x)
-                            self.removeStaleData(chanId)
-                        else:
-                            x[value] = data
-                            self.candleData[chanId].append(x)
-                            self.removeStaleData(chanId)
-                else:
-                    for value in data:
-                        x = {}
-                        if type(data[0]) == list:
-                            if value[0] > list(self.candleData[chanId][len(self.candleData[chanId]) - 1])[0]:
-                                x[value[0]] = value
-                                self.candleData[chanId].append(x)
-                                self.removeStaleData(chanId)
-                        else:
-                            if value > list(self.candleData[chanId][len(self.candleData[chanId]) - 1])[0]:
-                                x[value] = data
-                                self.candleData[chanId].append(x)
-                                self.removeStaleData(chanId)
-                chanKey = self.channelKeys[chanId].split(':')
-                time_frame = chanKey[1]
-                key = chanKey[2][1:]
-                strategies = examine_strategies(key, time_frame, self.candleData[chanId])
-                if strategies:
-                    self.update_strategy_list(self.channelKeys[chanId], strategies)
-
-    def on_error(self, error):
-        print(error)
-
-    def on_close(self):
-        print("### closed ###")
-
-    def on_open(self):
-        for key in constants.coinbase:
-            for time_frame in constants.time_frame:
-                self.ws.send('{' + constants.subscribe[id.ws_subscribe].format(time_frame, key.upper()) + '}')
-
-    def update_strategy_list(self, key, strategies):
-        append = True
-        for value in strategies:
-            strategy = list(value)[0]
-            if strategy not in strategyDict:
-                strategyDict[strategy] = [{key: value[strategy][id.price_action]}]
-            else:
-                for k in strategyDict[strategy]:
-                    if key in k:
-                        append = False
-                if append:
-                    strategyDict[strategy].append({key: value[strategy][id.price_action]})
-        print('Works')
-
-
-    def run(self):
-        websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp(constants.url[id.ws_candles],
-                                    on_message=self.on_message,
-                                    on_error=self.on_error,
-                                    on_close=self.on_close,
-                                    on_open=self.on_open)
-
-        self.ws.run_forever()
-
-class StrategyBroadcast(Thread):
-
-    def __init__(self, ps):
+    def __init__(self, ass):
         ''' Constructor. '''
         Thread.__init__(self)
-        self.popoServer = ps
+        self.AttendeeServer = ass
 
     def run(self):
         while True:
-            scheduler.enter(constants.scheduler_params[id.delay], constants.scheduler_params[id.priority], self.popoServer.broadcast)
+            scheduler.enter(constants.scheduler_params[id.delay], constants.scheduler_params[id.priority], self.AttendeeServer.broadcast)
             scheduler.run()
-
 
 if __name__ == "__main__":
 
-    ps = PopoServer()
-    ps.start()
-    bc = BitfinexClient()
-    bc.start()
-    sb = StrategyBroadcast(ps)
-    sb.start()
+    ass = AttendeeServer()
+    ass.start()
+    df = DataFetcher(ass)
+    df.start()
